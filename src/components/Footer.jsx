a5acb79cc612193d243be62b2f45ae3879f1f175
ddalla0425 <hiwgod92@gmail.com>
2025-11-16 15:35:17 +0900

    feat: add footer component responsive css and object array data separation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import {footerSite, footerContent} from "@/data/footerSite";
+import {useLanguage} from "@/hooks/useLanguage";
+
 export default function Footer() {
+    const lang = useLanguage();
+
     return (
-        <>
-            <div>푸터 영역</div>
-        </>
+        <footer>
+            <div className="subscribeBtn">
+                <Link href="/">{footerContent[lang].subscription}</Link>
+            </div>
+
+            <div>
+                <div className="footerLogo">
+                    <Image width={143} height={17} src="./assets/logo_white.svg" alt="yanolja research footer logo"/>
+                </div>
+                <div className="footerContent">
+                    <Link href="/">{footerContent[lang].privacy}</Link>
+                    <ul>
+                        {footerContent[lang].companyInfo.map((text, i) => (
+                            <li key={i}>{text}</li>
+                        ))}
+                    </ul>
+                    <ul>
+                        {footerContent[lang].contactInfo.map((text, i) => (
+                            <li key={i}>{i === 1 ? <a href="mailto:yanoljaresearch@yanolja.com">{text}</a> : text}</li>
+                        ))}
+                    </ul>
+                    <p>{footerContent[lang].copyright}</p>
+                </div>
+            </div>
+
+            <div className="selectFamilySite">
+                <select name="selectFamilySite">
+                    <option value="none">Family site</option>
+                    {footerSite.map(({href, siteName}) => (
+                        <option key={href} value={href}>{siteName}</option>
+                    ))}
+                </select>
+            </div>
+        </footer>
     );
 }
---
+++ src/data/footerSite.js
@@ -0,0 +1,37 @@
+// Footer 들어갈 데이터 배열
+export const footerContent = {
+        kr: {
+                subscription: "구독신청",
+                privacy: "개인정보 처리방침",
+                companyInfo: ["(주)야놀자리서치", "대표이사. 박성식", "사업자등록번호. 308-86-02788"],
+                contactInfo: [
+                        "Y-Siren (Compliance/Whistleblowing)",
+                        "E-mail. yanoljaresearch@yanolja.com",
+                        "서울특별시 강남구 테헤란로 108길 42 MDM타워 4층"
+                ],
+                copyright: "© YANOLJA RESEARCH. All rights reserved."
+        },
+        en: {
+                subscription: "subscribe",
+                privacy: "Privacy Policy",
+                companyInfo: ["Yanolja Research, Ltd.", "CEO Seongsik Park", "Lincense No. 308-86-02788"],
+                contactInfo: [
+                        "Y-Siren (Compliance/Whistleblowing)",
+                        "E-mail. yanoljaresearch@yanolja.com",
+                        "MDM Tower 4F, 42, Teheran-ro 108-gil, Gangnam-gu, Seoul, Republic of Korea"
+                ],
+                copyright: "© YANOLJA RESEARCH. All rights reserved."
+        }
+};
+export const footerSite = [
+        {href: "http://khta.khu.ac.kr/", siteName: "경희대 H&T 애널리틱스 센터"},
+        {href: "https://www.purdue.edu/hhs/htm/research/CHRIBA/index.html", siteName: "Purdue CHRIBA"},
+        {href: "https://www.yanolja.com/", siteName: "야놀자"},
+        {href: "https://business.yanolja.com/web/kr/solutions/solutions1", siteName: "야놀자클라우드"},
+        {href: "https://www.interpark.com/malls/index.html?gateTp=1", siteName: "인터파크"},
+        {href: "https://www.sanhait.co.kr/", siteName: "산하정보기술"},
+        {href: "https://triple.guide/", siteName: "트리플"},
+        {href: "https://www.dailyhotel.com/", siteName: "데일리호텔"},
+        {href: "https://dable.io/ko/", siteName: "데이블"},
+        {href: "https://www.ezeetechnosys.com/", siteName: "eZee Technosys"},
+    ]
