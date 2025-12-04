310a5f71a08959946759c3b18d588550493d3993
ddalla0425 <hiwgod92@gmail.com>
2025-12-05 02:12:04 +0900

    remove: delete files

--- src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-export default function MainContent() {
-    return (
-        <>
-            <div>메인 홈 영역</div>
-        </>
-    );
-}
--- src/hooks/useLanguage.js
+++
@@ -1,6 +0,0 @@
-import {useSearchParams} from "next/navigation";
-
-export function useLanguage(){
-    const params = useSearchParams();
-    return params.get("lang") || "kr";
-}
